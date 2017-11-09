3d7a39a46b923a7abca71713b3b73409f12b8359
EdwardN13 <33520732+EdwardN13@users.noreply.github.com>
2017-11-09 09:53:19 -0500

    Initial

---
+++ Mad Libs EN.py
@@ -0,0 +1,65 @@
+
+print("Write a verb ending in ing")
+verbing1 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write an adjective")
+adjective2 = input()
+
+print("Write a plural noun")
+pluralnoun1 = input()
+
+print("Write an adjective")
+adjective3 = input()
+
+print("Write a person in the room")
+personinroom1 = input()
+
+print("Write a plural noun")
+pluralnoun2 = input()
+
+print("Write a place")
+place1 = input()
+
+print("Write an adjective")
+adjective4 = "input"
+
+print("Write a celebrity")
+celebrity1 = input()
+
+print("Write a noun")
+noun1 = input()
+
+print("Write a plural noun")
+pluralnoun3 = input()
+
+print("Write an adjective")
+adjective5 = input()
+
+print("Write a plural noun")
+pluralnoun4 = input()
+
+print("Write a plural noun")
+pluralnoun5 = input()
+
+print("Write a noun")
+noun2 = input()
+
+print ("Write a plural noun")
+pluralnoun6 = input()
+
+
+### MAD LIB ###
+### Source : https://cdn-rainbowresource.netdna-ssl.com/products/022255i2.jpg
+
+print(" Espionage is a formal word for" + verbing1 + " . " )
+print(" In the shadowsy world of spies, a/an " + adjective1 + "organization like the US Government uses spies to inflitrate" + adjective2 + " groups for the purpose of obtaining top secret " + pluralnoun1 + ".")
+print(" For example, spies might have to crack the code for accesing confidential," + adjective3 + " files, or their mission could be far more dangerous- like stealing the key ingredient for making" + personinroom1 + "'s award-winning Explosive Fudgy" + pluralnoun2 + ".")
+print(" Spies are found all over (the) " + place1 + " - but they are not allowed to reveal their " + adjective4 + " identies.")
+print(" A teacher, " + celebrity1 + ", or even the little old " + noun1 + " with the cane and fifteen pet " + pluralnoun3 + " who lives next door to you could be a spy. ")
+print(" The world of spying may seem glamorous and" + adjective5 + "- but it's filled with risks and " + pluralnoun4 + "!")
+print(" Sure, spies have a never-ending supply of supercool electronic " + pluralnoun5 + ", but they can't trust any " + noun2 + "- wbich is why the number one rule of spies is to keep friend close - and keep " + pluralnoun6 + " closer!")
+    
+
